64a8f521eb7cbc321b7fbfb64ec8b6a4fa510816
jayzz21 <142303184+jayzz21@users.noreply.github.com>
2023-09-12 15:18:57 +0800

    Add files via upload

---
+++ Sample/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Visually Appealing Website</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to our Beautiful Website</h1>
+        <p>Your source for stunning web designs</p>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <section class="feature">
+            <h2>Our Services</h2>
+            <div class="service">
+                <h3>Web Design</h3>
+                <p>We create beautiful and functional websites.</p>
+            </div>
+            <div class="service">
+                <h3>Graphic Design</h3>
+                <p>We design eye-catching graphics for your brand.</p>
+            </div>
+            <div class="service">
+                <h3>SEO Optimization</h3>
+                <p>We help you rank higher in search engines.</p>
+            </div>
+        </section>
+        <section class="portfolio">
+            <h2>Portfolio</h2>
+            <div class="project">
+                <h3>Project 1</h3>
+            </div>
+            <div class="project">
+                <h3>Project 2</h3>
+            </div>
+            <div class="project">
+                <h3>Project 3</h3>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 Visually Appealing Website</p>
+    </footer>
+</body>
+</html>
